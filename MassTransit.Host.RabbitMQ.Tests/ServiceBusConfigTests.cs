df51d086f891757e002a13449ae3833c5685b8c2
Ron Griffin <rongriffin@gmail.com>
2014-08-11 15:49:01 -0500

    Unit testing for service bus configuration

---
+++ MassTransit.Host.RabbitMQ.Tests/ServiceBusConfigTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using MassTransit.Host.RabbitMQ.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MassTransit.Host.RabbitMQ.Tests
+{
+	/// <summary>
+	/// Unit Tests for ServiceBusConfigTests
+	/// </summary>
+	[TestClass]
+	public class ServiceBusConfigTests
+	{
+
+		/// <summary>
+		/// Verify that loading the config from the app.config works successfully
+		/// relies on this connection string in the app.config:
+		///		<add name="Host.RabbitMQConnection" connectionString="endpoint=rabbitmq://test/test; userId=testUser; password=testPassword" />
+		/// </summary>
+		[TestMethod]
+		public void ServiceBusConfigLoadFromConfigSuccess()
+		{
+			var result = ServiceBusConfig.LoadFromConfig();
+
+			Assert.IsNotNull(result);
+			Assert.AreEqual(result.EndpointAddress, "rabbitmq://test/test");
+			Assert.AreEqual(result.RabbitMqUserName, "testUser");
+			Assert.AreEqual(result.RabbitMqPassword, "testPassword");
+		}
+	}
+}
